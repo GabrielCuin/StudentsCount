4bf20028a717df44f937bb8d0016566e155a9457
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2020-09-20 16:13:49 -0300

    Projeto Finalizado

---
+++ Entities/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentsCount.Entities
+{
+    class Student
+    {
+        public string StudentName { get; set; }
+        public int StudentID { get; set; }
+
+        public Student(string studentName, int studentID)
+        {
+            StudentName = studentName;
+            StudentID = studentID;
+        }
+
+        public Student(int studentID)
+        {
+            StudentID = studentID;
+        }
+        public override int GetHashCode()
+        {
+            return StudentID.GetHashCode();
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Student))
+            {
+                return false;
+            }
+            Student other = obj as Student;
+            return StudentID.Equals(other.StudentID);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using StudentsCount.Entities;
 
 namespace StudentsCount
 {
@@ -6,7 +8,30 @@ namespace StudentsCount
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HashSet<Student> set = new HashSet<Student>();
+            Console.Write("How many students of course A? ");
+            int A = int.Parse(Console.ReadLine());
+            for (int i = 0; i < A; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new Student(x));
+            }
+            Console.Write("How many students of course B? ");
+            int B = int.Parse(Console.ReadLine());
+            for (int i = 0; i < B; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new Student(x));
+            }
+            Console.Write("How many students of course C? ");
+            int C = int.Parse(Console.ReadLine());
+            for (int i = 0; i < C; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new Student(x));
+            }
+            Console.Write("Total students: " + set.Count);
+            Console.ReadLine();
         }
     }
 }
